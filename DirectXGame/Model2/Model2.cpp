672682e71a7da5fcace807c34f5ddc81297c790b
tanabe-kazuma <k024g0040@g.neec.ac.jp>
2026-04-23 13:05:11 +0900

    四角形だした

--- DirectXGame/Model2/Model2.cpp
+++ DirectXGame/Model2/Model2.cpp
@@ -50,6 +50,7 @@ Model2* Model2::CreateFromOBJ(const std::string& modelname, bool smoothing) {
 }
 
 Model2* Model2::CreateSquare(int max) { 
+	
 	// メモリ確保
 	Model2* instance = new Model2;
 	std::vector<Mesh::VertexPosNormalUv> vertices;
@@ -67,20 +68,20 @@ Model2* Model2::CreateSquare(int max) {
 		int index = i * 4;
 		// 左下
 		vertices[index + 0].pos = {i * 2 + -1.0f, -1.0f, 0.0f};
-		vertices[index + 0].uv = {0, 1};
-		vertices[index + 0].normal = {0, 0, 1};
+		vertices[index + 0].uv = {0.0f, 1.0f};
+		vertices[index + 0].normal = {0.0f, 0.0f, 1.0f};
 		// 左上
 		vertices[index + 1].pos = {i * 2 + -2.0f, 1.0f, 0.0f};
-		vertices[index + 1].uv = {0, 0};
-		vertices[index + 1].normal = {0, 0, 1};
+		vertices[index + 1].uv = {0.0f, 0.0f};
+		vertices[index + 1].normal = {0.0f, 0.0f, 1.0f};
 		// 右下
 		vertices[index + 2].pos = {i * 2 + 1.0f, -1.0f, 0.0f};
-		vertices[index + 2].uv = {1, 1};
-		vertices[index + 2].normal = {0, 0, 1};
+		vertices[index + 2].uv = {1.0f, 1.0f};
+		vertices[index + 2].normal = {0.0f, 0.0f, 1.0f};
 		// 右上
 		vertices[index + 3].pos = {i * 2 + 2.0f, 1.0f, 0.0f};
-		vertices[index + 3].uv = {1, 0};
-		vertices[index + 3].normal = {0, 0, 1};
+		vertices[index + 3].uv = {1.0f, 0.0f};
+		vertices[index + 3].normal = {0.0f, 0.0f, 1.0f};
 	}
 
 	// インデックス
